d1f7ca76c760a7faf24091e4053b785358f5b880
Samuel Zuniga <sezuniga1@uc.cl>
2021-05-18 05:23:49 -0400

    Add exercise 1 leetcode

---
+++ module_1/arithmetic.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/add-digits/
+
+# A recursive solution for this problem. 
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        sum = 0
+        while num != 0:
+            sum+=num%10
+            num = (num-num%10)//10
+
+        if sum < 10:
+
+            return sum
+        else:
+            return self.addDigits(sum)
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.addDigits(38))
