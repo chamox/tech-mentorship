176a51188d1fc016796239e858452207e11e5b53
Samuel Zuniga <sezuniga1@uc.cl>
2021-05-19 12:34:36 -0400

    Add exercise 3: bitwise op

---
+++ module_1/hamming_distance.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        n = x^y
+        c=0
+        
+        while n !=0:
+            if n&1==1:
+                c+=1
+                n>>=1
+            else:
+                n>>=1
+        return c
+    
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.hammingDistance(1,4))
