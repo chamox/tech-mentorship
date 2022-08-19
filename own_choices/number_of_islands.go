70b7378e6b0f1ece1c2185537c9dda296c33c724
chamox <sezuniga1@uc.cl>
2022-08-19 15:13:12 -0400

    Add numIsland final golang implementation

--- own_choices/number_of_islands.go
+++ own_choices/number_of_islands.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"strconv"
 	"strings"
+	"log"
 )
 
 func check(e error) {
@@ -14,7 +14,7 @@ func check(e error) {
 	}
 }
 
-func numIslands2(grid [][]string) int {
+func numIslands(grid [][]string) int {
 	var queue [][]int
 	res := 0
 	for i := 0; i < len(grid); i++ {
@@ -72,7 +72,7 @@ func main() {
 		cols, err := strconv.Atoi(strings.Split(dirs[i].Name(), "_")[len(strings.Split(dirs[i].Name(), "_"))-1][0:1])
 
 		// Obtengo el id como string
-		// id := strings.Split(dirs[i].Name(), "_")[len(strings.Split(dirs[i].Name(), "_"))-2][0:1]
+		id := strings.Split(dirs[i].Name(), "_")[len(strings.Split(dirs[i].Name(), "_"))-2][0:1]
 
 		dat, err := os.ReadFile("matrices/" + dirs[i].Name())
 		check(err)
@@ -81,35 +81,20 @@ func main() {
 
 		rows := len(initial_array) / cols
 
-		// fmt.Print(string(dat))
-		// fmt.Print("\n")
-		// fmt.Print(initial_array)
-		// fmt.Print("\n")
-		// fmt.Printf("%d", cols)
-		// fmt.Print("\n")
-		// fmt.Printf("%d", rows)
-		// fmt.Print("\n")
-		// fmt.Print(string(id))
-		// fmt.Print("\n")
-
 		matrix := make([][]string, rows)
 
 		for i := 0; i < len(matrix); i++ {
 			matrix[i] = make([]string, cols)
 		}
 
-		// fmt.Println(matrix)
 
 		// ahora debo formar las matrices
-
-		////////////////////////////////////////
 
 		actual_row := 0
 		actual_col := 0
 
 		for i := 0; i < len(initial_array); i++ {
 			matrix[actual_row][actual_col] = initial_array[i]
-			// fmt.Print(initial_array[i])
 
 			if actual_col < cols-1 {
 				actual_col += 1
@@ -118,52 +103,24 @@ func main() {
 				actual_col = 0
 			}
 
-			// fmt.Printf("%d \n", actual_col)
-			// fmt.Printf("%d \n", actual_row)
-
 		}
 
-		fmt.Println(matrix)
+		res := numIslands(matrix)
 
-		res := numIslands2(matrix)
-		fmt.Println(res)
+		f, err := os.Create("respuestas/numberOfIslands_"+id+".txt")
 
-		////////////////////////////////////////
+		if err != nil {
+			log.Fatal(err)
+		}
+	
+		defer f.Close()
+	
+		_, err2 := f.WriteString(strconv.Itoa(res))
+	
+		if err2 != nil {
+			log.Fatal(err2)
+		}
 
-		// fmt.Println(dirs[i].Name())
-		// fmt.Println(dirs[i])
 	}
 
-	// f, err := os.Open("/tmp/dat")
-	// check(err)
-
-	// b1 := make([]byte, 5)
-	// n1, err := f.Read(b1)
-	// check(err)
-	// fmt.Printf("%d bytes: %s\n", n1, string(b1[:n1]))
-
-	// o2, err := f.Seek(6, 0)
-	// check(err)
-	// b2 := make([]byte, 2)
-	// n2, err := f.Read(b2)
-	// check(err)
-	// fmt.Printf("%d bytes @ %d: ", n2, o2)
-	// fmt.Printf("%v\n", string(b2[:n2]))
-
-	// o3, err := f.Seek(6, 0)
-	// check(err)
-	// b3 := make([]byte, 2)
-	// n3, err := io.ReadAtLeast(f, b3, 2)
-	// check(err)
-	// fmt.Printf("%d bytes @ %d: %s\n", n3, o3, string(b3))
-
-	// _, err = f.Seek(0, 0)
-	// check(err)
-
-	// r4 := bufio.NewReader(f)
-	// b4, err := r4.Peek(5)
-	// check(err)
-	// fmt.Printf("5 bytes: %s\n", string(b4))
-
-	// f.Close()
 }
