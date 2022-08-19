2d27bdf28b80553c844f9ba36e2aa86ca2d5b388
chamox <sezuniga1@uc.cl>
2022-08-18 23:12:01 -0400

    Add recursive function to island problem

--- own_choices/number_of_islands.go
+++ own_choices/number_of_islands.go
@@ -14,38 +14,65 @@ func check(e error) {
 	}
 }
 
+func numIslands2(grid [][]string) int {
+	var queue [][]int
+	res := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] == "1" {
+				queue = append(queue, []int{i, j})
+				for len(queue) > 0 {
+					row, col := queue[0][0], queue[0][1]
+					queue = queue[1:]
+					if row > 0 && grid[row-1][col] == "1" {
+						queue = append(queue, []int{row - 1, col})
+						grid[row-1][col] = "0"
+					}
+					if row < len(grid)-1 && grid[row+1][col] == "1" {
+						queue = append(queue, []int{row + 1, col})
+						grid[row+1][col] = "0"
+					}
+					if col > 0 && grid[row][col-1] == "1" {
+						queue = append(queue, []int{row, col - 1})
+						grid[row][col-1] = "0"
+					}
+					if col < len(grid[i])-1 && grid[row][col+1] == "1" {
+						queue = append(queue, []int{row, col + 1})
+						grid[row][col+1] = "0"
+					}
+				}
+				res++
+			}
+		}
+	}
+	return res
+}
+
 func main() {
 
-	// Con esto leemos el nombre de los archivos, para así generar la matriz
-
-	dirs, err := ioutil.ReadDir("matrices") // Leemos el directorio
-
+	// Leemos el nombre de los archivos
+	dirs, err := ioutil.ReadDir("matrices")
 	check(err)
 
-	// función para comprobar que estoy leyendo los directorios
-	// for i := 0; i < len(dirs); i++ {
-	// 	fmt.Println(dirs[i].Name())
-	// 	fmt.Println(strings.Split(dirs[i].Name(), "_"))
-	// 	fmt.Println(strings.Split(dirs[i].Name(), "_")[len(strings.Split(dirs[i].Name(), "_"))-1][0:1])
-	// }
-
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Leer todos los archivos ”.txt” que se encuentren en la carpeta ./matrices/. Se asume que la carpeta
 	// existe al ejecutar el programa. Los archivos seguiran el formato "matriz {id} {cols}.txt", siendo id el
 	// identificador unico de matriz y cols el numero de columnas de la matriz.
-
+	//
 	// El numero de filas se deduce con
 	// el tamano total dividido al numero de columnas. Si la carpeta esta vacia, levante una alarma y detenga la
 	// ejecucion. Los archivos siempre tendran matrices M x N validas.
-
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	for i := 0; i < len(dirs); i++ {
 
-		// obtengo el id y lo convierto a int
+		// Obtengo las columnas y las convierto a int
 		cols, err := strconv.Atoi(strings.Split(dirs[i].Name(), "_")[len(strings.Split(dirs[i].Name(), "_"))-1][0:1])
 
-		// obtengo el id como string
-		id := strings.Split(dirs[i].Name(), "_")[len(strings.Split(dirs[i].Name(), "_"))-2][0:1]
+		// Obtengo el id como string
+		// id := strings.Split(dirs[i].Name(), "_")[len(strings.Split(dirs[i].Name(), "_"))-2][0:1]
 
 		dat, err := os.ReadFile("matrices/" + dirs[i].Name())
 		check(err)
@@ -54,16 +81,16 @@ func main() {
 
 		rows := len(initial_array) / cols
 
-		fmt.Print(string(dat))
-		fmt.Print("\n")
-		fmt.Print(initial_array)
-		fmt.Print("\n")
-		fmt.Printf("%d", cols)
-		fmt.Print("\n")
-		fmt.Printf("%d", rows)
-		fmt.Print("\n")
-		fmt.Print(string(id))
-		fmt.Print("\n")
+		// fmt.Print(string(dat))
+		// fmt.Print("\n")
+		// fmt.Print(initial_array)
+		// fmt.Print("\n")
+		// fmt.Printf("%d", cols)
+		// fmt.Print("\n")
+		// fmt.Printf("%d", rows)
+		// fmt.Print("\n")
+		// fmt.Print(string(id))
+		// fmt.Print("\n")
 
 		matrix := make([][]string, rows)
 
@@ -71,7 +98,7 @@ func main() {
 			matrix[i] = make([]string, cols)
 		}
 
-		fmt.Println(matrix)
+		// fmt.Println(matrix)
 
 		// ahora debo formar las matrices
 
@@ -82,7 +109,7 @@ func main() {
 
 		for i := 0; i < len(initial_array); i++ {
 			matrix[actual_row][actual_col] = initial_array[i]
-			fmt.Print(initial_array[i])
+			// fmt.Print(initial_array[i])
 
 			if actual_col < cols-1 {
 				actual_col += 1
@@ -91,12 +118,15 @@ func main() {
 				actual_col = 0
 			}
 
-			fmt.Printf("%d \n", actual_col)
-			fmt.Printf("%d \n", actual_row)
+			// fmt.Printf("%d \n", actual_col)
+			// fmt.Printf("%d \n", actual_row)
 
 		}
 
 		fmt.Println(matrix)
+
+		res := numIslands2(matrix)
+		fmt.Println(res)
 
 		////////////////////////////////////////
 
